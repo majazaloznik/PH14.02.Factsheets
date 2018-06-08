04a9881116a8a4ef10877380d04e2a9718932fad
majazaloznik <maja.zaloznik@gmail.com>
2018-06-08 18:10:42 +0100

    no more jaggedy populations - except Tunisia

--- code/02-clean-data.R
+++ code/02-clean-data.R
@@ -14,76 +14,74 @@ source(here::here("code/FunSpline.R"))
 ## 01. data import  =========================================================
 
 # import population data 
-mena.pop <- readRDS("data/processed/mena.pop.rds")
+pop <- readRDS("data/processed/mena.pop.rds")
 
 # import life expectacy data 
-mena.lt <- readRDS("data/processed/mena.lt.rds")
+lt <- readRDS("data/processed/mena.lt.rds")
 
-## 02. data culling  ==========================================================
-
-# get proportion ove 65 in each country/year combination
-mena.pop %>% 
-  select(Location, Time, PopTotal, AgeGrp) %>% 
-  mutate(over.65 = ifelse(AgeGrp < 65, "under", "over")) %>% 
-  group_by(Location, Time, over.65) %>% 
-  summarise(PopTotal = sum(PopTotal)) %>% 
-  spread(over.65, PopTotal) %>% 
-  mutate(prop.over.65 = over/(over+under))  -> mena.over.65
   
 ## 03. data interpolation =====================================================
 
 # use splines to get old age threshold (15 years remaining life expectancy)
 # for each year/country combination
-mena.lt %>% 
+lt %>% 
   group_by(Location, MidPeriod, Sex) %>% 
   summarise(old.age=FunSpline(ex,AgeGrpStart, 15))  %>% 
-  spread(key = Sex, value = old.age)  -> mena.old.age
+  spread(key = Sex, value = old.age)  -> old.age.threshold.5y
 
 # use splines to get the age thresholds for each year
-
 # first expand to get all years needed
-interpolating.years <- expand.grid(MidPeriod = seq(from=min(mena.old.age$MidPeriod),
-                                                   to = max(mena.old.age$MidPeriod), by = 1),
-                                   Location = unique(mena.old.age$Location))
+interpolating.years <- expand.grid(MidPeriod = seq(from=min(old.age.threshold.5y$MidPeriod),
+                                                   to = max(old.age.threshold.5y$MidPeriod), by = 1),
+                                   Location = unique(old.age.threshold.5y$Location))
 
 # interpolate the threshold for each single year, for all three Sex groups
-mena.old.age %>% 
+old.age.threshold.5y %>% 
   right_join(interpolating.years) %>% 
   group_by(Location) %>% 
   mutate(Female.old.age.i = spline(MidPeriod, Female, xout = MidPeriod)$y,
          Male.old.age.i = spline(MidPeriod, Male, xout = MidPeriod)$y,
          Total.old.age.i = spline(MidPeriod, Total, xout = MidPeriod)$y) -> 
-  mena.old.age.single.year
-  
+  old.age.threshold.1y
+rm(interpolating.years, old.age.threshold.5y)
+
 # now merge that back with the full population table, sliding them as extra rows in
-mena.old.age.single.year %>% 
+old.age.threshold.1y %>% 
   select(Location, MidPeriod, Total.old.age.i) %>% 
   rename(AgeGrp = Total.old.age.i, Time = MidPeriod) %>% 
   mutate(threshold = AgeGrp) %>% 
-  bind_rows(mena.pop) %>% 
+  bind_rows(pop) %>% 
   select(c(1:4, 11)) %>% 
   arrange(Location, Time,AgeGrp) %>% 
   group_by(Location, Time) %>% 
-  mutate(PopTotal = spline(AgeGrp, PopTotal, xout = AgeGrp)$y) %>% 
+  mutate(PopTotal = ifelse(is.na(PopTotal), 
+                           threshold %%1 * lag(PopTotal), PopTotal)) %>% 
   mutate(temp = ifelse(!is.na(threshold), lag(PopTotal), NA),
          PopTotal = ifelse(!is.na(lead(threshold)), lead(PopTotal), PopTotal),
          PopTotal = ifelse(!is.na(threshold), temp-PopTotal, PopTotal)) %>% 
   select(-temp) %>% 
   arrange(Location, Time, threshold) %>% 
   fill(threshold) %>% 
-  arrange(Location, Time,AgeGrp)  %>% 
-  mutate(over.threshold = ifelse(AgeGrp < threshold, "under", "over")) %>% 
+  arrange(Location, Time,AgeGrp)  -> pop.old.age.threshold.1y
+
+
+# now summarise proportions over threshold and over 65
+pop.old.age.threshold.1y %>% 
+  mutate(over.65 = ifelse(AgeGrp < 65, "under", "over"),
+         over.threshold = ifelse(AgeGrp < threshold, "under", "over")) %>% 
+  group_by(Location, Time, over.65) %>% 
+  mutate(Pop.65 = sum(PopTotal)) %>% 
   group_by(Location, Time, over.threshold) %>% 
-  summarise(PopTotal = sum(PopTotal)) %>% 
-  spread(over.threshold, PopTotal) %>% 
-  mutate(over.threshold = over/(over+under))  -> mena.over.threshold
-# 
-# 
-# left_join(mena.over.65, mena.over.threshold, by = c("Location" = "Location", "Time" = "Time")) -> x
-# 
-# x %>% 
-#   filter(Location == "Oman") -> a
-#   
-# plot(a$Time, a$prop.over.65, type = "l", xlim = c(1980, 2050), lwd = 2, ylim = c(0, 0.2)) 
-# lines(a$Time, a$over.threshold, lwd = 2, lty = 2, col = "red") 
-#           
+  mutate(Pop.threshold = sum(PopTotal)) %>% 
+  group_by(Location, Time) %>% 
+  summarise(under.t = first(Pop.threshold),
+            over.t = last(Pop.threshold),
+            under.65 = first(Pop.65),
+            over.65 = last(Pop.65)) %>% 
+  mutate(prop.over.65 = over.65/(over.65+under.65),
+         prop.over.t = over.t/(over.t+under.t))  -> prop.over
+
+
+saveRDS(prop.over, "data/processed/prop.over.rds")
+saveRDS(old.age.threshold.1y, "data/processed/threshold.1y.rds")
+
--- code/03-plotting.R
+++ code/03-plotting.R
@@ -1,4 +1,18 @@
+##=============================================================================
+## 00. preliminaries ==========================================================
+## 01. plotting thresholds ====================================================
+## 02. plotting proportions over 65 ===========================================
+##=============================================================================
 
+
+
+## 00. preliminaries ==========================================================
+prop.over <- readRDS("data/processed/prop.over.rds")
+threshold.1y <-readRDS("data/processed/threshold.1y.rds")
+library(dplyr)
+
+
+prop.over
 
 
 FunPlotOldAge <- function(country = "Algeria", 
@@ -7,8 +21,8 @@ FunPlotOldAge <- function(country = "Algeria",
                           col.total = "palevioletred4",
                           col.male = "palevioletred3",
                           col.female = "palevioletred3"){
-  mena.old.age.single.year %>% 
-    filter(MidPeriod > 1980, MidPeriod < 2050) -> mena.old.age.single.year
+  threshold.1y %>% 
+    filter(MidPeriod > 1980, MidPeriod < 2050) -> threshold.1y
   par(bg = col.bg)
   par(mar = c(3,3,1,0))
   plot(2000, 50, 
@@ -18,7 +32,7 @@ FunPlotOldAge <- function(country = "Algeria",
        type = "n",
        ylab = "", xlab = "",
        axes = FALSE)
-  mena.old.age.single.year %>% 
+  threshold.1y %>% 
     group_by(Location) %>% 
     lines(Total.old.age.i ~ MidPeriod, . ,
           lwd = 3, col = col.20)
@@ -26,17 +40,17 @@ FunPlotOldAge <- function(country = "Algeria",
     lines(c(1980, 2050), c(i,i), col = "gray50", lty = 3)
   }
   
-  mena.old.age.single.year %>% 
+  threshold.1y %>% 
     filter(Location == country) %>% 
     lines(Total.old.age.i ~ MidPeriod, . ,
           lwd = 3, col = col.total )
   
-  mena.old.age.single.year %>% 
+  threshold.1y %>% 
     filter(Location == country) %>% 
     lines(Male.old.age.i ~ MidPeriod, . ,
           lwd = 3, col = col.male, lty = 1)
   
-  mena.old.age.single.year %>% 
+  threshold.1y %>% 
     filter(Location == country) %>% 
     lines(Female.old.age.i ~ MidPeriod, . ,
           lwd = 3, col = col.female, lty = 1)
@@ -56,3 +70,24 @@ FunPlotOldAge("Egypt")
 FunPlotOldAge("Algeria")
 FunPlotOldAge("Tunisia")
 FunPlotOldAge("Turkey")
+
+country = "Egypt"
+par(mar = c(3,3,1,0))
+plot(2000, 0, 
+     xlim = c(1980, 2050), 
+     ylim = c(0, .2),
+     bty = "n",
+     type = "n",
+     ylab = "", xlab = "",
+     axes = FALSE)
+prop.over%>% 
+  filter(Location== country) %>% 
+  lines(prop.over.65 ~ Time, . ,
+        lwd = 3) 
+prop.over%>% 
+  filter(Location== country) %>% 
+  lines(prop.over.t ~ Time, . ,
+        lwd = 3, col = "red") 
+axis(2, pos = 1980, las = 2)
+axis(1)
+  
